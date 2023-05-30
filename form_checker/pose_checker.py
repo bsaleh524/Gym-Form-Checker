24402bbb5c3fd5e2fc1280853301df14ee452f7a
Basem Saleh <mreagles524@gmail.com>
2023-05-29 23:00:48 -0400

    Used ai website to help with labeling

--- form_checker/pose_checker.py
+++ form_checker/pose_checker.py
@@ -2,40 +2,50 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import os
-
-
-
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+import pandas as pd
 
 
 class form_checker():
+    '''
+    Class that collects data from multiple deadlifting videos and
+    creates a pandas dataframe for each individual frame that includes:
+    - file name
+    - Pose locations
+    - Lifting stage label (Provided by user)
+    - Form label (Provided by user)
 
+    
+    '''
     def __init__(self,
                  detect_conf: int = 0.5,
                  track_conf: int = 0.5):
-        ''' Initialize our class that holds our Pose model parameters.'''
+        
+        '''Initialize our class that holds our Pose model parameters.'''
 
         self.pose_drawing = mp.solutions.drawing_utils
         self.pose_tools = mp.solutions.pose
         self.pose_model = self.pose_tools.Pose(min_detection_confidence=detect_conf,
                                                min_tracking_confidence=track_conf)
+        
+        # initialize dataframe for data storage
+        pose_name_array = [pose_name.name for _, pose_name in enumerate(self.pose_tools.PoseLandmark)] + ["video_file", "lifting_stage", "form_check"]
+        self.dataframe = pd.DataFrame(columns=pose_name_array)
 
     def check(self, video_file: str):
         '''Open given video file,
         play video file with poses on top.'''
         capture = cv2.VideoCapture(video_file)
-        with self.pose_model as PM:
-            while capture.isOpened():
-                _, frame = capture.read()
-
+        print(f"Video Loaded")
+        while capture.isOpened():
+            success, frame = capture.read()
+            if success:
                 # Recolor out image from BGR to RGB. MediaPose takes in RGB
                 # and be default, cv2 outputs as a BGR.
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 image.flags.writeable = False # Prevent video feed from being written to when converting
 
                 ## Make Detection. Get detections and store in `results`
-                results = PM.process(image)
+                results = self.pose_model.process(image)
 
                 ## Recolor back to BGR to show on the feed
                 image.flags.writeable = True # Allow the current frame to be written over, now that we have detections
@@ -46,12 +56,12 @@ class form_checker():
                     landmarks = results.pose_landmarks.landmark
 
                     #### Get Coordinates
-                    left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                    left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                    left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                    left_shoulder = [landmarks[self.pose_tools.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[self.pose_tools.PoseLandmark.LEFT_SHOULDER.value].y]
+                    left_elbow = [landmarks[self.pose_tools.PoseLandmark.LEFT_ELBOW.value].x, landmarks[self.pose_tools.PoseLandmark.LEFT_ELBOW.value].y]
+                    left_wrist = [landmarks[self.pose_tools.PoseLandmark.LEFT_WRIST.value].x, landmarks[self.pose_tools.PoseLandmark.LEFT_WRIST.value].y]
                 
                     #### Calculate angle
-                    angle = self.calculate_angle(left_shoulder, left_elbow, left_wrist)
+                    angle = self._calculate_angle(left_shoulder, left_elbow, left_wrist)
 
                     #### Visualize our value to the screen
                     cv2.putText(image, str(angle),
@@ -64,22 +74,96 @@ class form_checker():
 
 
                 ## Render our detections onto the image
-                mp_drawing.draw_landmarks(image,
+                self.pose_drawing.draw_landmarks(image,
                                         results.pose_landmarks, # Pass specific landmark coordinates
-                                        mp_pose.POSE_CONNECTIONS,
-                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
-                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)) # Pass connections(right shoulder --> right elbow)
+                                        self.pose_tools.POSE_CONNECTIONS,
+                                        self.pose_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                        self.pose_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)) # Pass connections(right shoulder --> right elbow)
 
                 cv2.imshow('Mediapipe Feed', image)
 
                 # For exiting the live feed
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
+            else:
+                print("Video Ended")
+                capture.release()
+                cv2.destroyAllWindows()
+                break
 
-            capture.release()
-            cv2.destroyAllWindows()
 
-    def _calculate_angle(a,b,c):
+    def collect_data(self,
+                     video_directory: str):
+        '''
+        This function will open our video file and append a new row
+        to the dataframe
+        '''
+
+        # iterate over files in
+        # that directory
+        for filename in os.listdir(video_directory):
+            file_path = os.path.join(video_directory, filename)
+            abspath = os.path.abspath(file_path)
+            # checking if it is a file
+            if os.path.isfile(abspath):
+                
+                # Open video file
+                capture = cv2.VideoCapture(abspath)
+
+                while capture.isOpened():
+                    success, frame = capture.read()
+                    if success:
+                        # Recolor out image from BGR to RGB. MediaPose takes in RGB
+                        # and be default, cv2 outputs as a BGR.
+                        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        image.flags.writeable = False # Prevent video feed from being written to when converting
+
+                        ## Make Detection. Get detections and store in `results`
+                        results = self.pose_model.process(image)
+
+                        ## Recolor back to BGR to show on the feed
+                        image.flags.writeable = True # Allow the current frame to be written over, now that we have detections
+                        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                        ### Extract Landmarks if present
+                        # try:
+                        # Get all joint positions
+                        landmarks = results.pose_landmarks.landmark
+                            
+                        ## Render our detections onto the image
+                        self.pose_drawing.draw_landmarks(image,
+                                                results.pose_landmarks, # Pass specific landmark coordinates
+                                                self.pose_tools.POSE_CONNECTIONS,
+                                                self.pose_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                                self.pose_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)) # Pass connections(right shoulder --> right elbow)
+
+                        ## Ask user for labeling:
+
+                        ## DOWN, UP, or LIFTING?
+
+                        ## GOOD_FORM or BAD_FORM
+
+                        ## Append all info to dataframe
+                        
+                        # except:
+                        #     pass
+
+
+                        
+
+                        cv2.imshow('Mediapipe Feed', image)
+
+                        # For exiting the live feed
+                        if cv2.waitKey(10) & 0xFF == ord('q'):
+                            break
+                    else:
+                        print("Video Ended")
+                        capture.release()
+                        cv2.destroyAllWindows()
+                        break
+
+
+    def _calculate_angle(self, a,b,c):
         """
         Calculate angle between three joints
         a: first joint: default = 11(left shoulder)
@@ -104,12 +188,4 @@ if __name__ == '__main__':
     
     # Init our class
     AI = form_checker()
-    directory = "form_checker/web_scraper/reddit_videos/"
-    # iterate over files in
-    # that directory
-    for filename in os.listdir(directory):
-        fullfile_path = os.path.join(directory, filename)
-        # checking if it is a file
-        if os.path.isfile(fullfile_path):
-            AI.check(fullfile_path)
-    
+    AI.collect_data("form_checker/web_scraper/reddit_videos/")
